176c69924df68ab828c41f84441ac567c91dd151
Syed Wassi Ul Haque <wassi244255@gmail.com>
2021-02-28 20:27:12 +0600

    until useEffect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [nayoks, setNayoks]=useState([])
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then (res => res.json())
+    .then (data => setNayoks(data));
+
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {
+         nayoks.map( nk => <Nayok name={nk.name} age={nk.age}></Nayok>)
+       }
+        
       </header>
     </div>
   );
+}
+
+function Nayok (props) {
+  const nayokStyle = {
+    border: '2px solid purple',
+    margin : '20px'
+  }
+  return (
+   <div style={nayokStyle}>
+     <h1>I'm Hero : {props.name} </h1>
+     <h3>I Have done 5 movies in {props.age} years </h3>
+    </div>
+  )
 }
 
 export default App;
